2f5b60578bf0f11472a9395a97374fdf152b5ae1
lolo <playback85@gmail.com>
2015-09-10 10:40:34 +0200

    Old web

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lolo
+ * Date: 18/1/15
+ * Time: 11:41
+ */
+
+//include "app/config.php";
+//include "app/detect.php";
+
+/*if ($page_name=='') {
+    include $browser_t.'/index.html';
+}
+elseif ($page_name=='index.html') {
+    include $browser_t.'/index.html';
+}
+elseif ($page_name=='single-page.html') {
+    include $browser_t.'/single-page.html';
+}
+elseif ($page_name=='contact.html') {
+    include $browser_t.'/contact.html';
+}
+elseif ($page_name=='contact-post.html') {
+    include $browser_t.'/contact.html';
+    include 'app/contact.php';
+}
+else
+{
+    include $browser_t.'/404.html';
+}*/
+
+include 'web/index.html';
+
+?>
+
